9d24d45732be555356b7ebe6edfee5ba4515ed6c
Samyak <samyakanand2004@gmail.com>
2024-08-15 12:25:07 +0530

    changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,3 @@
-// import type { Config } from "tailwindcss";
-
-// const config: Config = {
-//   content: [
-//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-//   ],
-//   theme: {
-//     extend: {
-//       colors: {
-//         black: {
-//           DEFAULT: '#000',
-//           100: '#000319'
-//         }
-//       }
-//     },
-//   },
-//   plugins: [],
-// };
-// export default config;
-
 import type { Config } from "tailwindcss";
 
 const svgToDataUri = require("mini-svg-data-uri");
